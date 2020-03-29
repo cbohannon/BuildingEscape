3bf00a72589aee1c4ad5550a0c48c64662c75330
Chris Bohannon <chrisbohannon@gmail.com>
2020-03-29 14:59:44 -0500

    Added a Grabber class abd BP

--- BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
+++ BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBuildingEscape_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xBD856D82,
-				0x95971355,
+				0x720FCCB4,
+				0x403D64BE,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
